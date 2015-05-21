e0a331dda204004ee050847e5c904d5b8a6e103b
Jay Zhu <tingjie.zhu@gmail.com>
2015-05-20 22:10:53 -0700

    bug fix

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 set -e
-# set -x
+set -x
 
-DIR=$( dirname "${BASH_SOURCE[0]}" )
+DIR=`cd $( dirname "${BASH_SOURCE[0]}" ); pwd`
 TEMP_DIR=${DIR}/temp
 
 function install() {
@@ -35,7 +35,7 @@ fi;
 my_config_file=$HOME/.oh-my-zsh/lib/my_configs.zsh
 if [ ! -f $my_config_file ]; then
   echo "Set up zsh"
-  ln -s ~/dotfiles/my_configs.zsh $my_config_file
+  ln -s $DIR/my_configs.zsh $my_config_file
 else
   echo "~/.oh-my-zsh/custom/my_configs.zsh already exists. Skipping ... "
 fi;
@@ -57,14 +57,14 @@ sh $font_dir/install.sh
 
 if [ ! -f $HOME/.vim_runtime/my_configs.vim ]; then
   echo "Set up vim"
-  ln -s ~/dotfiles/vimrc ~/.vim_runtime/my_configs.vim
+  ln -s $DIR/vimrc ~/.vim_runtime/my_configs.vim
 else
   echo "~/.vim_runtime/my_configs.vim already exist. Skipping ... "
 fi;
 
 if [ ! -f $HOME/.tmux.conf ]; then
   echo "Set up tmux"
-  ln -s ~/dotfiles/tmux ~/.tmux.conf
+  ln -s $DIR/tmux ~/.tmux.conf
 else
   echo "~/.tmux.conf already exists. Skipping ... "
 fi;
